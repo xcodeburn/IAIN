ad4b8f0e399fba9e7a4ed787b8fe00df0e957063
Fredy <xnameburn@gmail.com>
2025-01-09 10:36:35 +0700

    menambah test pada page blog

--- iain-mod/resources/views/about.blade.php
+++ iain-mod/resources/views/about.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.main') <!-- halaman ini menggunakan layout ini-->
 @section('container') <!-- ini yang akan dipanggil pada main.blade-->
 <h1>ini | about</h1>
+<h2>Ini | test</h2>
 @endsection
