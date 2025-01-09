a5f9229174eff7accf92bbce3fd1344693f2c90b
Fredy <xnameburn@gmail.com>
2025-01-09 11:13:53 +0700

    menghapus beberapa folder dalam master

--- MobilBekas/resources/views/post.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.main')
-
-@section('container')
-    <article>
-        <h2>{{$bp["title"]}}</h2>
-        <h2>{{$bp["author"]}}</h2>
-        <p>{{$bp["body"]}}</p>
-    </article>
-@endsection
