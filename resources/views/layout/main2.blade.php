c6a889fc076919b87f6744f7e7a2c8f417bbee9e
Fredy <xnameburn@gmail.com>
2025-01-17 22:19:21 +0700

    menambah navbar v2 -> selain home

---
+++ resources/views/layout/main2.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en"> <!-- data-bs-theme="light" -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style2.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>IAIN | {{ $title }}</title>
+</head>
+<body>
+@include('partials.navbar2')
+@yield('container') <!-- ini nama yang akan diisi pada menu-menu nav-->
+@include('partials.footer')
+<script src="{{asset('js/script.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/partials/navbar2.blade.php
@@ -0,0 +1,79 @@
+<nav class="navbar navbar-expand-lg bg-green">
+        
+    <div class="container" style="font-weight:500;">
+            
+            <!-- <a class="navbar-brand" style="color: #ecd31f;" href="#">IAIN</a> -->
+            
+            <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
+                <div class="d-flex align-items-center">
+                    <a href="" class="navbar-brand d-flex align-items-center" style='color:white;'>
+                        <img src="img/IAIN.png" alt="" class="me-2" width="50" height="70">
+                        <div>
+                            <span class="d-block fw-bold">INSTITUT</span>
+                            <span class="d-block fw-bold">AGAMA ISLAM</span>
+                            <span class="d-block fw-bold">ROKAN</span>
+                        </div>
+                    </a>
+                </div>
+                
+            <ul class="navbar-nav ms-auto">
+                   
+            <li class="nav-item mx-3">
+                <a class="nav-link {{$title == 'home' ? 'active' : ''}}" style="color: #ecd31f;" href="/">Sistem Informasi</a>
+                <!-- Jika tekan tombol home (title == home), maka kasih tanda -->
+                </li>
+                <li class="nav-item mx-3">
+                <a class="nav-link {{ $title == 'blog' ? 'active' : ''}}" style="color: #ecd31f;" href="/blog">PDDIKTI</a>
+                </li>
+                <li class="nav-item mx-3">
+                <a class="nav-link {{ $title == 'about' ? 'active' : ''}}" style="color: #ecd31f;" href="/about">Aduan & Saran</a>
+                </li>
+            </ul>
+            <i class="fa fa-search" style="font-size:20px"></i>   
+            </div>
+        </div>
+</nav>
+{{-- <img src="img/IAIN.png" alt="" class="iain-logo"> --}}
+<nav class="navbar navbar-expand-lg">
+    <div class="container" style="font-weight:500; text-transform:uppercase;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-lg-0">
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link active">home</a>
+        </li>
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link active">profil</a>
+        </li>
+       <!-- dropdown -->
+       <div class="dropdown2" style="font-weight: 500;text-transform:lowecase;">
+            <button data-mdb-button-init data-mdb-ripple-init data-mdb-dropdown-init class="btn dropdown-toggle" style="font-weight: 500;text-transform:uppercase; type="button" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
+                fakultas
+            </button>
+            <ul class="dropdown-menu" style="color:#fff;" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item my-3" href="#">Action</a></li>
+                <li><a class="dropdown-item my-3" href="#">Another action</a></li>
+                <li><a class="dropdown-item my-3" href="#">Something else here</a></li>
+            </ul>
+        </div>
+       <!-- dropdown -->
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link">unit</a>
+        </li>
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link">akademik</a>
+        </li>
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link">penerimaan</a>
+        </li>
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link">mahasiswa</a>
+        </li>
+        <li class="nav-item mx-3">
+            <a href="" class="nav-link">penerimaan</a>
+        </li>
+    </ul>
+    </div>
+</nav>
