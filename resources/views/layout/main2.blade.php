037e070bb069c46f749ff1084cdff4627ba706c8
Fredy <xnameburn@gmail.com>
2025-01-26 08:03:03 +0700

    menambah no-page

--- resources/views/layout/main2.blade.php
+++ resources/views/layout/main2.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/style2.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>IAIN | {{ $title }}</title>
+    <title>IAIR | {{ $title }}</title>
 </head>
 <body>
 @include('partials.navbar2')
---
+++ resources/views/no-page.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main2') <!-- halaman ini menggunakan layout ini-->
+@section('container') <!-- ini yang akan dipanggil pada main.blade-->
+<div class="container-fluid">
+    <img src="/img/no-page.webp" alt="no-page" class="no-page">
+</div>
+@endsection
