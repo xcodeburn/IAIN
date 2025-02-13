e605e6ffab05a1c55e4dadb014e68ceefdf65c49
Fredy <xnameburn@gmail.com>
2025-02-13 10:10:58 +0700

    penambahan dan perubahan minor pada vie abot, hone dan footer

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,32 @@
 @extends('layout.main2') <!-- halaman ini menggunakan layout ini-->
+<style>
+    img {
+    /* width: 100%; */
+    max-width: 520px;
+    float: right ;
+    height: auto;
+    border-radius: 8px;
+}
+</style>
 @section('container2') <!-- ini yang akan dipanggil pada main.blade-->
     <div class="container about">
         <div class="content mt-3">
             <!-- Sejarah Sekolah -->
+            <img src="/img/sekolah.jpg" alt="Gambar Sekolah" class="school-image">
             <div class="left">
                 <h2>Sekilas Tentang IAI-Rokan</h2>
-                <p>Sekolah ini didirikan pada tahun 1990 dengan tujuan untuk menyediakan pendidikan berkualitas bagi anak-anak di wilayah ini. Seiring berjalannya waktu, sekolah ini berkembang pesat dengan fasilitas yang lebih lengkap dan staf pengajar yang berkompeten.</p>
+                <p>
+                    Institut Agama Islam Rokan (IAIR) awalnya dikenal sebagai Sekolah Tinggi Agama Islam (STAI) Rokan Bagan Batu, yang didirikan pada 15 April 2000. Lembaga ini berkomitmen memberikan kesempatan pendidikan tinggi bagi lulusan SLTA, Pesantren, dan Madrasah. Kehadiran STAI Rokan Bagan Batu merupakan impian masyarakat Muslim di Kecamatan Bagan Sinembah serta umat Islam di Kabupaten Rokan Hilir secara umum.</p>
+                <p>
+                    Untuk menjaga kualitas pendidikan, IAIR memastikan bahwa seluruh dosen yang mengajar adalah profesional yang kompeten di bidangnya masing-masing. Meskipun relatif muda, IAIR Bagan Batu terus menunjukkan perkembangan yang signifikan, terbukti dengan peningkatan jumlah mahasiswa setiap tahunnya.
+                </p>
+                <p>
+                    Saat ini, IAIR Bagan Batu mengelola empat program studi unggulan, yaitu:<br> 1) Pendidikan Agama Islam (PAI)</br><br> 2) Pendidikan Islam Anak Usia Dini (PIAUD)</br><br> 3) Pendidikan Guru Madrasah Ibtidaiyah (PGMI)></br><br> 4) Ekonomi Syariah.</br>
+                </p>
             </div>
             <!-- Gambar -->
             <div class="right">
-                <img src="/img/sekolah.jpg" alt="Gambar Sekolah" class="school-image">
+                
             </div>
         </div>
     
