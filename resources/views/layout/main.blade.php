8128a33481f21bf336ad9932dc64fda00675348a
Fredy <xnameburn@gmail.com>
2025-02-22 05:48:40 +0700

    modifikasi navbar, home,style2, dan main page

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -19,5 +19,6 @@
 <!-- Hanya menyertakan Bootstrap Bundle -->
 {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
