ebdce27719a7768e20e451e330e7b94d4f1951ea
Fredy <xnameburn@gmail.com>
2025-02-21 22:02:17 +0700

    perubahan pada footer dan home page, serta modifikasi style2

---
+++ resources/views/partials/footer2.blade.php
@@ -0,0 +1,84 @@
+<!-- Calon footer -->
+<div class="contact-section">
+    <div class="container-fluid">
+        <div class="row">
+            <img src="{{ asset('img/IAIN.png') }}" alt="IAIR" class="iain-logo-footer">
+            <div class="alamat">
+                <h1>Institut Agama Islam<br> ROKAN</h1>
+            </div>
+             <div class="col-md-4">
+                <div class="sosial">
+                    <a href="" class="text-white">maps</a>
+                </div>
+
+                <!-- Follow section dipindah di bawah maps -->
+                <!-- Follow section diubah agar teks dan ikon sosial media berdampingan -->
+                <div class="follow-section d-flex align-items-center mt-4">
+                    <p class="follow-text mb-0 mr-3" style="white-space: nowrap;">FOLLOW US</p> <!-- Margin kanan agar ada jarak antara teks dan ikon -->
+                    <div class="social-icons d-flex">
+                        <a href="" target="_blank" class="mx-2">
+                            <img src="{{ asset('img/yt.png') }}" alt="youtube" >
+                        </a>
+                        <a href="" target="_blank" class="mx-2">
+                            <img src="{{ asset('img/ins.png') }}" alt="instagram">
+                        </a>
+                        <a href="" target="_blank" class="mx-2">
+                            <img src="{{ asset('img/ins.png') }}" alt="facebook">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-5 contact-info text-white">
+            <p>
+                Jl. Lintas riau - sumut, km. 6 bagan batu<br>
+                bahtera makmur, kec. bagan sinembah,<br>
+                kab. rokan hilir, prov. riau<br><br></p>
+                <table style="color:white;" >
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 822 8839 4221</td>
+                        <td>Dr. ahmad hafizon, M.Pd</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 813 7183 8763</td>
+                        <td>fery sudarmanto, SE., S.Pd.I</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 813 9617 0447</td>
+                        <td>isro sidri ridho hasibuan, S.Pd.I</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 853 6324 7004</td>
+                        <td>ilham hasibuan, S.Pd</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 853 5591 6292</td>
+                        <td>ahmad arifin, SE</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 822 6847 3172</td>
+                        <td>mar'i roisuddin, S.Pd</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-phone" style="font-size:25px"></i></td>
+                        <td>+62 823 7379 4265</td>
+                        <td>novi arianti, S.IP</td>
+                    </tr>
+                </table>
+            </p>
+        </div>
+    </div>
+</div>
+
+<footer class="bg-dark text-white py-3">
+    <div class="container text-center">
+        <p>&copy; 2025 Institut Agama Islam Rokan. All Rights Reserved. Developed by IAI Rokan</p>
+    </div>
+</footer>
